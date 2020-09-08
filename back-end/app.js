43bd3d44bb975256fd1daf1fa5d306f9085de240
GangbacOl <gangyeonjo336@gmail.com>
2020-09-09 00:41:17 +0900

    [ADD] 백엔드 개발환경 초기 세팅.

---
+++ back-end/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const cookiepParser = require('cookie-parser');
+const models = require('./models');
+const app = express();
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiepParser());
+
+app.get('/test', (req, res) => {
+    console.log('test success');
+    res.status(200).json({ message: 'success' });
+});
+
+models.sequelize
+    .sync()
+    .then(() => {
+        console.log('Success to connect to DB.');
+    })
+    .catch((err) => {
+        console.log('Fail to connect to DB.');
+        console.log(err);
+    });
+
+app.listen(5000, (req, res) => {
+    console.log('server successfully start');
+});
