dc8bbbbef3157e79cf95c0bb1bd71fd8fcb7d78c
GangbacOl <gangyeonjo336@gmail.com>
2020-09-14 02:38:58 +0900

    [UPD] 작성한 모델 등록.

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -1,12 +1,13 @@
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
-var config = require('../config/config.json');
+var config = require('../config/sequelize_config.json');
 var db = {};
 var sequelize = new Sequelize(config[env].database, config[env].username, config[env].password, config[env]);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-db.User = require('./user')(sequelize, Sequelize);
-db.Board = require('./board')(sequelize, Sequelize);
+db.user = require('./user')(sequelize, Sequelize);
+db.board = require('./board')(sequelize, Sequelize);
+db.email_verified_status = require('./email_verified_status')(sequelize, Sequelize);
 
 module.exports = db;
